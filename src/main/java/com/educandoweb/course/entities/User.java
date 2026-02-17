a526ff1a1d5cc8cf2d3d1c13a46985eca6af1c3a
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2026-02-17 10:53:20 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,9 +1,11 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jdk.jfr.Name;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,10 @@ public class User implements Serializable {
     private String email;
     private String password;
     private String phone;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -69,6 +75,10 @@ public class User implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -1,23 +1,23 @@
 package com.educandoweb.course.services;
 
-import com.educandoweb.course.entities.User;
-import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService{
+public class OrderService {
 
     @Autowired
-    private UserRepository repository;
+    private OrderRepository repository;
 
-    public List<User> findAll(){
+    public List<Order> findAll(){
         return repository.findAll();
     }
 
-    public User findById(Long id){
+    public Order findById(Long id){
         return repository.findById(id).orElseThrow(() -> new RuntimeException("Usuário, não encontrado!"));
     }
 }
